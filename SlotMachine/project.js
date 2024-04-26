0d84ac225af7955d2d906432ee4cbb7ff9318dee
Parth Srivastav <parthsrivastav.00@gmail.com>
2024-04-26 23:37:19 +0530

    Added the Spin method for the Slot Machine

--- SlotMachine/project.js
+++ SlotMachine/project.js
@@ -8,6 +8,22 @@
 
 const prompt = require("prompt-sync")();
 
+const ROWS = 3;
+const COLS = 3;
+
+const SYMBOLS_COUNT = {
+  A: 2,
+  B: 4,
+  C: 6,
+  D: 8
+}
+const SYMBOLS_VALUES = {
+  A: 5,
+  B: 4,
+  C: 3,
+  D: 2
+}
+
 const deposit = () => {
   while (true) {
     const depositAmount = prompt("Enter your deposit: ");
@@ -34,5 +50,35 @@ const getNumberOfLines = () => {
   }
 }
 
-let depositAmount = deposit();
+const getBet = (balance, lines) => {
+  while (true) {
+    const bet = prompt("Enter the bet per line: ");
+    const numberBet = parseFloat(bet);
+
+    if (isNaN(numberBet) || numberBet <= 0 || numberBet > (balance / lines)) {
+      console.log("Invalid bet, enter again");
+    } else {
+      return numberBet;
+    }
+  }
+}
+
+const spin = () => {
+  const symbols = [];
+  for (const[symbol, count] of Object.entries(SYMBOLS_COUNT)) {
+    for (let i = 0; i < count; i++) {
+      symbols.push(symbol);
+    }
+  }
+
+  const reels = [[], [], []];
+  for (let i = 0; i < COLS; i++) {
+    for (let j = 0; j < ROWS; j++) {
+
+    }
+  }
+}
+
+let balance = deposit();
 const numberOfLines = getNumberOfLines();
+const bet = getBet(balance, numberOfLines);
