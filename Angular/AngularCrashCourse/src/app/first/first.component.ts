821d33b9ea0c09580dcb1b9c068eb530f6bc0ef1
Parth Srivastav <parthsrivastav.00@gmail.com>
2024-04-29 00:25:29 +0530

    Implement form submission and message deletion in FirstComponent

--- Angular/AngularCrashCourse/src/app/first/first.component.ts
+++ Angular/AngularCrashCourse/src/app/first/first.component.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {FormsModule} from "@angular/forms";
+import {NgForOf, NgIf} from "@angular/common";
+import {MessageDetailsComponent} from "../message-details/message-details.component";
 
 @Component({
-  selector: 'app-first',
-  standalone: true,
-  imports: [],
-  templateUrl: './first.component.html',
-  styleUrl: './first.component.scss'
+    selector: 'app-first',
+    standalone: true,
+    imports: [
+        FormsModule,
+        NgIf,
+        NgForOf,
+        MessageDetailsComponent
+    ],
+    templateUrl: './first.component.html',
+    styleUrl: './first.component.scss'
 })
 export class FirstComponent {
+    name: string = '';
+    email: string = '';
+    message: string = '';
+    isSubmitted: boolean = false;
+    messages: Array<any> = [];
 
+    onSubmit() {
+        this.isSubmitted = true;
+
+        this.messages.push({
+            'name': this.name,
+            'email': this.email,
+            'message': this.message
+        });
+
+        console.log(this.messages)
+    }
+
+    deleteMessage(index: number) {
+        this.messages.splice(index, 1);
+    }
 }
